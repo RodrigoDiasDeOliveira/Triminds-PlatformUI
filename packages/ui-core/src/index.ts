97267749d0139c2afae471163690ac5daf2a6d5d
rodrigo dias de oliveira <rodrigo@email.com>
2026-06-03 13:31:01 +0200

    structure four

--- packages/ui-core/src/index.ts
+++ packages/ui-core/src/index.ts
@@ -1,13 +1,7 @@
-// Core exports
-export * from './theme/ThemeProvider'
-export * from './theme/createTheme'
-export * from './theme/useTheme'
-export * from './theme/types'
-
-// Layout
-export * from './layout/AppShell'
-export * from './layout/Sidebar'
-export * from './layout/Header'
-
-// Components
-export * from './components/ui'
+// packages/ui-core/src/index.ts
+export * from './theme';
+export * from './components';
+export * from './layout';
+export * from './patterns';
+export * from './hooks';
+export * from './utils';
